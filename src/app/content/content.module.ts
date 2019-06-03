dc647ea91396f8232d5c5fcde2520c075856d99a
La Treta <yves_henry13@hotmail.com>
2019-06-03 03:14:16 -0300

    exibicao de filtros e produtos

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -4,11 +4,12 @@ import { ProdutosComponent } from './produtos/produtos.component';
 import { LojinhasComponent } from './lojinhas/lojinhas.component';
 import { AjudaComponent } from './ajuda/ajuda.component';
 import { BemvindoComponent } from './bemvindo/bemvindo.component';
+import { FiltrosComponent } from './produtos/filtros/filtros.component';
 
 @NgModule({
     imports: [CommonModule],
     exports: [ProdutosComponent, LojinhasComponent, AjudaComponent, BemvindoComponent],
-    declarations: [ProdutosComponent, LojinhasComponent, AjudaComponent, BemvindoComponent],
+    declarations: [ProdutosComponent, LojinhasComponent, AjudaComponent, BemvindoComponent, FiltrosComponent],
     providers: []
 })
 export class ContentModule {}
