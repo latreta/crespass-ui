1f09fa0b4221710f26257f329e62ff500a957b6a
latreta <yves_henry13@hotmail.com>
2020-04-12 02:23:02 -0300

    Model and screens just for display

--- src/app/content/produtos/components/view-product/view-product.component.ts
+++ src/app/content/produtos/components/view-product/view-product.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Produto } from 'src/app/core/model';
 
 @Component({
   templateUrl: './view-product.component.html'
 })
 export class ViewProductComponent implements OnInit {
   private id: any;
+  private produto: Produto;
 
   constructor(activeRoute: ActivatedRoute) {
     this.id = activeRoute.snapshot.params.id;
--- src/app/core/model.ts
+++ src/app/core/model.ts
@@ -1,4 +1,25 @@
 export interface Produto {
     id?: number;
+    store?: string;
+    category?: string;
+    brand: string;
     name: string;
+    description: string;
+    status: string;
+    quality: string;
+    originalPrice: string;
+    quantity: number;
+    weight: number;
+    height: number;
+    width: number;
+    targets: string;
+    createdAt: string;
+}
+
+export interface Usuario {
+    id?: number;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    
 }
